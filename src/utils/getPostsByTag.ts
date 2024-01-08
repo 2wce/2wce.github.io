5ffa0432784faf81d0052ccf3dc854594473935f
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2024-01-08 09:23:54 +0100

    fix: resolve single post

--- src/utils/getPostsByTag.ts
+++ src/utils/getPostsByTag.ts
@@ -2,9 +2,14 @@ import type { CollectionEntry } from "astro:content";
 import getSortedPosts from "./getSortedPosts";
 import { slugifyAll } from "./slugify";
 
-const getPostsByTag = (posts: CollectionEntry<"blog">[], tag: string) =>
-  getSortedPosts(
+const getPostsByTag = (posts: CollectionEntry<"blog">[], tag: string) => {
+  if (!posts) {
+    return [];
+  }
+
+  return getSortedPosts(
     posts.filter(post => slugifyAll(post.data.tags).includes(tag))
   );
+};
 
 export default getPostsByTag;
--- src/utils/getReadingTime.ts
+++ src/utils/getReadingTime.ts
@@ -1,14 +1,6 @@
 import { toString } from "mdast-util-to-string";
 import getReadingTime from "reading-time";
 
-interface Data {
-  astro: {
-    frontmatter: {
-      readingTime: string;
-    };
-  };
-}
-
 export function remarkReadingTime() {
   return function (tree: any, { data }: { data: any }) {
     const textOnPage = toString(tree);
